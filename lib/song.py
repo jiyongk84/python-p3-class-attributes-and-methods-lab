212792c6c3e85a52480abdd5bc1e7d401669bd4e
Jiyong Kim <luke.jiyongkim@gmail.com>
2023-07-21 17:01:31 -0400

    Completed lab

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,21 @@
 class Song:
-    pass
+    songs = []
+    genres = set()
+    artists = set()
+    genre_count = {}
+    artist_count = {}
+    count = 0
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        
+        Song.songs.append(self)
+
+        Song.genres.add(genre)
+        Song.artists.add(artist)
+        Song.genre_count[genre] = Song.genre_count.get(genre, 0) + 1
+        Song.artist_count[artist] = Song.artist_count.get(artist, 0) + 1
+
+        Song.count += 1
